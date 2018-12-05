7ce7b9c61ac83f96e4a9a86b9cd679f71e2af957
Unknown <duong.eric07@gmail.com>
2018-12-05 13:23:33 +0100

    No repeat search

--- app/src/main/java/chefmeal/chefmeal/SearchIngredientsActivity.java
+++ app/src/main/java/chefmeal/chefmeal/SearchIngredientsActivity.java
@@ -1,20 +1,35 @@
 package chefmeal.chefmeal;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 import java.util.ArrayList;
+import java.util.List;
+
+import static android.content.ContentValues.TAG;
 
 public class SearchIngredientsActivity extends AppCompatActivity {
 
     private ArrayAdapter<String> adapter;
     private ArrayList<String> arrayList;
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private List<String> listIngre = new ArrayList<String>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,16 +40,49 @@ public class SearchIngredientsActivity extends AppCompatActivity {
         adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList);
         list.setAdapter(adapter);
 
+
+        db.collection("Ingredients")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()){
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                listIngre.add(document.getId().toString());
+                            }
+                        }else{
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                        System.out.println(listIngre.toString());
+                    }
+                });
     }
 
     public void addIngredients(View view){
         EditText newIngredient = (EditText) findViewById(R.id.editText2);
         if(!(newIngredient.getText().toString().matches(""))){
-            arrayList.add(newIngredient.getText().toString());
-            adapter.notifyDataSetChanged();
+            if(arrayList.isEmpty()){
+                arrayList.add(newIngredient.getText().toString());
+                adapter.notifyDataSetChanged();
+            }else{
+                Boolean alreadyExist = false;
+                for (String i:arrayList) {
+                    if (newIngredient.getText().toString().matches(i)){
+                        alreadyExist = true;
+                    }
+                }
+                if (!(alreadyExist)){
+                    arrayList.add(newIngredient.getText().toString());
+
+                    adapter.notifyDataSetChanged();
+                }else{
+                    Toast.makeText(this, "Tu as déjà rentré cet ingrédient", Toast.LENGTH_SHORT).show();
+                }
+            }
         }
         else{
-            Toast.makeText(this, "You did not enter anything", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Tu n'as rien écris.", Toast.LENGTH_SHORT).show();
         }
     }
 
